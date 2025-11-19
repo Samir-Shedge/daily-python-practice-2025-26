4e4cdcf91801fcba83d053a290e6e1c526df519a
Samir-Shedge <samirshedge4153@gmail.com>
2025-11-19 13:20:05 +0530

    added progrmas - 19 Nov 2025

---
+++ program_06.py
@@ -0,0 +1,10 @@
+# Write a Python program to convert kilometers to miles.
+
+print("--- Distance Converter (kilometers to miles) ---")
+
+
+km_value = float(input("Enter a distance in kilometers: "))
+
+miles_value = km_value * 0.621371
+
+print(f"{km_value} kilometer is equals to {miles_value} miles.")
---
+++ program_07.py
@@ -0,0 +1,12 @@
+# Write a Python program to convert Celsius to Fahrenheit.
+
+print("--- Temprature Converter (Celsius to Fahrenheit) ---")
+
+celsius = float(input("Enter temprature in distance: "))
+fahrenheit_1 = 33.8
+
+result = celsius * fahrenheit_1
+
+print(f"{celsius} celsius is equals to {result} fahrenheit")
+
+
