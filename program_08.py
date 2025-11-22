d701beaeed2b886f581d408a99e495e80ffb6dc7
Samir-Shedge <samirshedge4153@gmail.com>
2025-11-22 18:57:22 +0530

    program 8 added - 22 Nov 2025

---
+++ program_08.py
@@ -0,0 +1,14 @@
+# Write a program to display a calender
+
+import calendar
+
+print(calendar.calendar(2025)) #print the calendar of that year
+
+# print(calendar.month(2025,9)) #print the calendar of that month
+
+# print(calendar.isleap(2025))
+# print(calendar.isleap(2024))
+
+# print(calendar.weekday(2025, 1, 31)) #Shows the week numbers
+
+# print(calendar.monthrange(2025, 12))
